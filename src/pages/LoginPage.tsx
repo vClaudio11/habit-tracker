5f710a8c9bcaf84017aa1b457eaab02257849103
vClaudio11 <vonclaudio.college@gmail.com>
2026-06-23 14:35:29 +0800

    feat: add base UI for login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,16 +1,58 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
 import { useState } from "react"
 
-interface LoginCardProps {
+interface LoginPageProps {
     onLogin: () => void
 }
 
-export default function LoginPage({ onLogin }: LoginCardProps) {
-    const [ email, setEmail ] = useState()
-    const [ password, setPassword ] = useState()
+export default function LoginPage({ onLogin }: LoginPageProps) {
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
 
     return(
-        <div>
-            
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="w-[390px] h-[844px] overflow-y-hidden overflow-x-hidden rounded-3xl shadow-2xl border border-gray-800 bg-background">
+                <Card className="justify-center h-full w-full">
+                    <CardHeader className="text-center">
+                        <CardTitle>Log in</CardTitle>
+                        <CardDescription>Login to your Habits account</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <FieldGroup>
+                            <Field>
+                                <Label>Email</Label>
+                                <Input 
+                                    placeholder="name@example.com"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </Field>
+                            <Field>
+                                <Label>Password</Label>
+                                <Input 
+                                placeholder="••••••••"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </Field>
+                            <Field>
+                                <Button 
+                                    variant="outline" 
+                                    onClick={() => {}}>
+                                    Log in
+                                </Button>
+                            </Field>
+                        </FieldGroup>
+                    </CardContent>
+                    <CardFooter>
+                        {/* "Don't have an account? Sign up" */}
+                    </CardFooter>
+                </Card>
+            </div>
         </div>
     )
 }
