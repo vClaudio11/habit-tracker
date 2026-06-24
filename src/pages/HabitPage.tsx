d4636344e375468726db767f135451d39abf041a
vClaudio11 <vonclaudio.college@gmail.com>
2026-06-24 10:46:44 +0800

    feat: add ternary loading state for database fetch on mount

--- src/pages/HabitPage.tsx
+++ src/pages/HabitPage.tsx
@@ -33,6 +33,8 @@ import {
 } from "@/components/ui/table"
 import HabitRadialChart from "../components/HabitCards/HabitRadialChart"
 import HabitBarChart from "../components/HabitCards/HabitBarChart"
+import { Item, ItemContent, ItemMedia, ItemTitle } from "@/components/ui/item"
+import { Spinner } from "@/components/ui/spinner"
 
 
 
@@ -70,8 +72,10 @@ export default function HabitPage() {
   const completed = habits.filter(h => h.completed).length
   const total = habits.length
   const [weeklyLog, setWeeklyLog] = useState<DailyLog[]>([])
+  const [loading, setLoading] = useState(true)
   
   
+
   // Ensure that array is seeded with 7 "" array elements
   useEffect(() => {
     const fetchWeeklyLog = async () => {
@@ -106,6 +110,7 @@ export default function HabitPage() {
           body: JSON.stringify(seed)
         })
 
+        setLoading(false)
       } else {
         setWeeklyLog(data)
       }
@@ -115,11 +120,6 @@ export default function HabitPage() {
   }, [])
 
 
-
-  // save to localStorage on changes
-  useEffect(() => {
-    localStorage.setItem("weeklyLog", JSON.stringify(weeklyLog))
-  }, [weeklyLog])
 
   useEffect(() => {
     localStorage.setItem("habits", JSON.stringify(habits))
@@ -180,78 +180,91 @@ export default function HabitPage() {
   }
 
     return(
-        <div className="flex flex-col items-center justify-center min-h-screen"> 
-    {/* Hardcode the mobile layout UI */}
-      <div className="w-[390px] h-[844px] overflow-y-hidden overflow-x-hidden rounded-3xl shadow-2xl border border-gray-800 bg-background">
-        <Card>
-          <CardHeader>
-            <CardTitle>Habit tracker</CardTitle>
-            <CardDescription>Log your habits</CardDescription>
-          </CardHeader>
-          <CardContent>
-            {/* Charts */}
-            <div className="grid grid-cols-2 gap-4 w-full mb-4">
-                <HabitRadialChart completed={completed} total={total}/>   
-                <HabitBarChart weeklyLog={weeklyLog}/>
+      <div className="flex flex-col items-center justify-center min-h-screen"> 
+      {/* Hardcode the mobile layout UI */}
+        <div className="w-[390px] h-[844px] overflow-y-hidden overflow-x-hidden rounded-3xl shadow-2xl border border-gray-800 bg-background">
+          {loading ? (
+            <div className="flex justify-center items-center h-full w-full [--radius:1rem]">
+              <Item variant='muted' className="flex flex-col justify-center items-center text-center">
+                <ItemMedia>
+                  <Spinner />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle className="line-clamp-1">Loading your habits...</ItemTitle>
+                </ItemContent>
+              </Item>
             </div>
-            <Tabs defaultValue='To-do' className='w-auto'>
-                <TabsList>
-                    <TabsTrigger value='to-do'>To-do</TabsTrigger>
-                    <TabsTrigger value='create'>Create</TabsTrigger>
-                    <TabsTrigger value='edit'>Edit</TabsTrigger>
-                </TabsList>
-                {/* To-do tab */}
-                <TabsContent value='to-do'>
-                    <Card className="overflow-y-auto">
-                        <CardHeader>
-                          <FieldGroup className='min-h-82'>
-                            <Table className="table-fixed w-full">
-                              <TableHeader>
-                                <TableRow>
-                                    <TableHead className='w-[70%]'>Habit</TableHead>
-                                    <TableHead className='w-[30%]'>Type</TableHead>
-                                </TableRow>
-                              </TableHeader>
-                            <TableBody>
-                              {habits.map(habit => (
-                                <TableRow key={habit.id}>
-                                    <HabitTodoCard habits={habit} onToggle={toggleHabit}/>
-                                </TableRow>
-                                ))}
-                              </TableBody>
-                            </Table>
-                          </FieldGroup>
-                        </CardHeader>
-                    </Card>
-                </TabsContent>
-                {/* Create tab */}
-                <TabsContent value='create'>
-                    <Card>
-                        <CardHeader>
-                          <FieldGroup className='min-h-82'>
-                            <HabitCreateCard onAdd={handleAdd}/>
-                          </FieldGroup>
-                        </CardHeader>
-                    </Card>
-                </TabsContent>
-                {/* Edit tab */}
-                <TabsContent value='edit'>
-                    <Card className="min-h-90 max-h-90 overflow-y-auto">
-                      <CardContent>
-                        <Field className="gap-y-4">
-                            {habits.map(habit => (
-                              <HabitEditCard key={habit.id} habits={habit} onDelete={handleDelete} onEdit={handleEdit}/>
-                            ))}                
-                        </Field>
-                      </CardContent>
-                    </Card>
-                </TabsContent>
-            </Tabs>
-          </CardContent>
-          <CardFooter>
-          </CardFooter>
-        </Card>
+          ) : (
+            <Card>
+              <CardHeader>
+                <CardTitle>Habit tracker</CardTitle>
+                <CardDescription>Log your habits</CardDescription>
+              </CardHeader>
+              <CardContent>
+                {/* Charts */}
+                <div className="grid grid-cols-2 gap-4 w-full mb-4">
+                    <HabitRadialChart completed={completed} total={total}/>   
+                    <HabitBarChart weeklyLog={weeklyLog}/>
+                </div>
+                <Tabs defaultValue='To-do' className='w-auto'>
+                    <TabsList>
+                        <TabsTrigger value='to-do'>To-do</TabsTrigger>
+                        <TabsTrigger value='create'>Create</TabsTrigger>
+                        <TabsTrigger value='edit'>Edit</TabsTrigger>
+                    </TabsList>
+                    {/* To-do tab */}
+                    <TabsContent value='to-do'>
+                        <Card className="overflow-y-auto">
+                            <CardHeader>
+                              <FieldGroup className='min-h-82'>
+                                <Table className="table-fixed w-full">
+                                  <TableHeader>
+                                    <TableRow>
+                                        <TableHead className='w-[70%]'>Habit</TableHead>
+                                        <TableHead className='w-[30%]'>Type</TableHead>
+                                    </TableRow>
+                                  </TableHeader>
+                                <TableBody>
+                                  {habits.map(habit => (
+                                    <TableRow key={habit.id}>
+                                        <HabitTodoCard habits={habit} onToggle={toggleHabit}/>
+                                    </TableRow>
+                                    ))}
+                                  </TableBody>
+                                </Table>
+                              </FieldGroup>
+                            </CardHeader>
+                        </Card>
+                    </TabsContent>
+                    {/* Create tab */}
+                    <TabsContent value='create'>
+                        <Card>
+                            <CardHeader>
+                              <FieldGroup className='min-h-82'>
+                                <HabitCreateCard onAdd={handleAdd}/>
+                              </FieldGroup>
+                            </CardHeader>
+                        </Card>
+                    </TabsContent>
+                    {/* Edit tab */}
+                    <TabsContent value='edit'>
+                        <Card className="min-h-90 max-h-90 overflow-y-auto">
+                          <CardContent>
+                            <Field className="gap-y-4">
+                                {habits.map(habit => (
+                                  <HabitEditCard key={habit.id} habits={habit} onDelete={handleDelete} onEdit={handleEdit}/>
+                                ))}                
+                            </Field>
+                          </CardContent>
+                        </Card>
+                    </TabsContent>
+                </Tabs>
+              </CardContent>
+              <CardFooter>
+              </CardFooter>
+            </Card>
+        )}
+        </div>
       </div>
-    </div>
     )
 }
